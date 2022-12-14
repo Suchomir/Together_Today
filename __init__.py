48eeced3207ac1e674d5e80f47b0b004b9a5f6ee
Suchomir <03.suchy@gmail.com>
2022-12-14 23:02:56 +0100

    Add essential files and start installing tailwind

---
+++ __init__.py
@@ -0,0 +1,45 @@
+#EDIT blueprints
+
+import os
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_migrate import Migrate, upgrade
+from flask_login import LoginManager
+
+
+def create_app(config="together_today.config.Config"):
+    app = Flask(__name__, static_folder="dist", static_url_path="/static")
+
+    with app.app_context():
+        app.config.from_object(config)
+
+        toolbar = DebugToolbarExtension()
+        toolbar.init_app(app)
+
+        # pylint: disable=import-outside-toplevel
+        from together_today.models import db
+
+        db.init_app(app)
+        Migrate(app, db)
+        upgrade()
+
+        login_manager = LoginManager()
+        login_manager.login_view = "auth.login"
+        login_manager.init_app(app)
+
+        from .models import User
+
+        @login_manager.user_loader
+        def load_user(user_id: int):
+            return User.query.get(int(user_id))
+
+        # pylint: disable=import-outside-toplevel
+        from together_today.views import general, auth, admin, profile, post
+
+        app.register_blueprint(general)
+        app.register_blueprint(auth)
+        app.register_blueprint(admin)
+        app.register_blueprint(profile)
+        app.register_blueprint(post)
+
+    return app
---
+++ config.py
@@ -0,0 +1,69 @@
+import os
+
+#EDIT uploads folder
+
+def strtobool(val):
+    val = val.lower()
+
+    if val in ("y", "yes", "t", "true", "on", "1"):
+        return 1
+
+    if val in ("n", "no", "f", "false", "off", "0"):
+        return 0
+
+    raise ValueError(f"invalid truth value {val}")
+
+
+def bool_cast(value):
+    if isinstance(value, bool):
+        return value
+
+    if value is None:
+        return False
+
+    if value == "":
+        return None
+
+    return bool(strtobool(value))
+
+
+def gen_secret_key():
+    # Attempt to read the secret from the secret file
+    # This will fail if the secret has not been written
+    try:
+        with open(".together_today_secret_key", "rb") as secret:
+            key = secret.read()
+    except OSError:
+        key = None
+
+    if not key:
+        key = os.urandom(64)
+        # Attempt to write the secret file
+        # This will fail if the filesystem is read-only
+        try:
+            with open(".together_today_secret_key", "wb") as secret:
+                secret.write(key)
+                secret.flush()
+        except OSError:
+            pass
+
+    return key
+
+
+class Config:
+    DEBUG: bool = bool_cast(os.environ.get("DEBUG", True))
+    SECRET_KEY: str = os.environ.get("SECRET_KEY", gen_secret_key())
+    BASE_DIR: str = os.path.dirname(os.path.abspath(__file__))
+
+    DATABASE_URL: str = os.environ.get(
+        "DATABASE_URL",
+        f"sqlite:///{BASE_DIR}/together_today.db",
+    )
+
+    UPLOADS_FOLDER = os.path.join(BASE_DIR, "uploads", "profile_pictures")
+
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    SQLALCHEMY_TRACK_MODIFICATIONS = bool_cast(
+        os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS", False)
+    )
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
